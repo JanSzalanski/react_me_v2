ee2318a20eb2f4cc1e855a997ee4b26b753f3779
JanSzalanski <jan@szalanski.eu>
2022-01-20 22:09:35 +0100

    added styled components

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  color: black;
+  padding: 10px;
+  background-color: #ffd82b;
+  width: 100px
+  height: 25px;
+`;
+
+export default Button;
---
+++ src/view/Root/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../../components/Button';
+
+function Root() {
+  return (
+    <div>
+      <h1>react_me v2</h1>
+      <Button>My Button</Button>
+    </div>
+  );
+}
+
+export default Root;
