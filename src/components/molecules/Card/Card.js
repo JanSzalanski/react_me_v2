d0f1b84e3c856f53482488fb7b428abc485a9a00
JanSzalanski <jan@szalanski.eu>
2022-02-01 23:21:53 +0100

    added Card component

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -2,17 +2,25 @@ import React from 'react';
 import classes from './Card.module.css';
 import CardUI from '../../UI/CardUI';
 import Heading from '../../atoms/Heading/Heading';
-import Pargraph from '../../atoms/Paragraph/Paragraph';
+import Paragraph from '../../atoms/Paragraph/Paragraph';
+import Button from '../../atoms/Button/Button';
 
 const Card = (props) => {
+  const widthFlex = classes.wrapper + ' flex';
   return (
-    <CardUI className={classes[props.classNme] || classes.card}>
-      <Heading>Karta AAA</Heading>
-      <Pargraph>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos adipisci dolorem magni
-        distinctio inventore qui odio aut illum corrupti, id expedita unde, error odit eaque
-        consectetur accusantium ex neque ipsam?{' '}
-      </Pargraph>
+    <CardUI className={classes[props.className] || classes.card}>
+      <div className={classes.wrapperColor}>
+        <Heading>Karta AAA</Heading>
+        <Paragraph className="paragraphB">3 days</Paragraph>
+      </div>
+      <div className={widthFlex}>
+        <Paragraph>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos adipisci dolorem
+          magni distinctio inventore qui odio aut illum corrupti, id expedita unde, error odit eaque
+          consectetur accusantium ex neque ipsam?{' '}
+        </Paragraph>
+        <Button className="secondary">Button</Button>
+      </div>
     </CardUI>
   );
 };
