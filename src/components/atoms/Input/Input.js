ed272c39f4081bf64c2f4cd839e0933d425d01a6
JanSzalanski <jan@szalanski.eu>
2022-02-13 23:40:13 +0100

    added style for input component

--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -3,12 +3,17 @@ import classes from './Input.module.css';
 
 const Input = (props) => {
   return (
-    <input
-      className={classes[props.className] || classes.input}
-      placeholder={props.placeholder || 'search'}
-      type="text"
-      //   onClick={props.onClick}
-    />
+    <div className={classes.wrapperAll}>
+      <div className={classes.backWrapper}>
+        <div className={classes.background}></div>
+      </div>
+      <input
+        className={classes[props.className] || classes.input}
+        placeholder={props.placeholder || 'search'}
+        type="text"
+        //   onClick={props.onClick}
+      />
+    </div>
   );
 };
 
