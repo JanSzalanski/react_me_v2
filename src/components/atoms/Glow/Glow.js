8208b982d1fd29f76b60036e6b402d34e37825ef
JanSzalanski <jan@szalanski.eu>
2022-02-15 22:47:43 +0100

    experiments...

---
+++ src/components/atoms/Glow/Glow.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './Glow.module.css';
+
+const Glow = (props) => {
+  return <div className={classes.glow}></div>;
+};
+
+export default Glow;
--- src/components/organisms/SideBar/SideBar.js
+++ src/components/organisms/SideBar/SideBar.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import classes from './SideBar.module.css';
 import Menu from '../../molecules/Menu/Menu';
+import Glow from '../../atoms/Glow/Glow';
 import PropTypes from 'prop-types';
 
 const SideBar = ({ pageType }) => {
   return (
     <div className={classes.wrapper}>
       <div className={classes.background}></div>
-      {pageType === 'news' && <div className={classes.glow}></div>}
-      {pageType === 'article' && <div className={classes.glowA}></div>}
-      {pageType === 'people' && <div className={classes.glowB}></div>}
-      {pageType === 'nft' && <div className={classes.glowC}></div>}
+      {pageType === 'news' && <Glow />}
+      {pageType === 'article' && <Glow style={{ top: '170px' }} />}
+      {pageType === 'people' && <Glow />}
+      {pageType === 'nft' && <Glow />}
       <div className={classes.sidebar}></div>
       <Menu />
     </div>
