1b63f1d8c6c188bc403b409b0e6cfb408031ca01
JanSzalanski <jan@szalanski.eu>
2022-02-22 23:06:54 +0100

    added styles for header

--- src/components/molecules/Header/Header.js
+++ src/components/molecules/Header/Header.js
@@ -4,7 +4,17 @@ import Heading from '../../atoms/Heading/Heading';
 
 const Header = (props) => {
   return (
-    <header class={classes[props.className] || classes.wrapperHeader}>
+    <header className={classes[props.className] || classes.wrapperHeader}>
+      <div className={classes.line1}></div>
+      <div className={classes.line2}></div>
+      <div className={classes.line3}></div>
+      <div className={classes.line4}></div>
+      <div className={classes.line5}></div>
+      <div className={classes.line6}></div>
+      <div className={classes.line7}></div>
+      <div className={classes.line8}></div>
+      <div className={classes.line9}></div>
+      <div className={classes.line10}></div>
       <Heading>{props.children}</Heading>
     </header>
   );
