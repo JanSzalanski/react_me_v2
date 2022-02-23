61c6bdf93b74414e01d94932bc5ff8fac26e1331
JanSzalanski <jan@szalanski.eu>
2022-02-23 16:25:19 +0100

    added styles fot footer

--- src/components/molecules/Footer/Footer.js
+++ src/components/molecules/Footer/Footer.js
@@ -5,6 +5,16 @@ const Footer = (props) => {
   return (
     <footer class={classes[props.className] || classes.wrapperFooter}>
       <h2 className={classes.footerTitle}>{props.children}</h2>
+      <div className={classes.line1}></div>
+      <div className={classes.line2}></div>
+      <div className={classes.line3}></div>
+      <div className={classes.line4}></div>
+      <div className={classes.line5}></div>
+      <div className={classes.line6}></div>
+      <div className={classes.line7}></div>
+      <div className={classes.line8}></div>
+      <div className={classes.line9}></div>
+      <div className={classes.line10}></div>
     </footer>
   );
 };
