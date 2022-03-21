c87a3ae1a1086dd60eae533c1f2f13b30757aa18
JanSzalanski <jan@szalanski.eu>
2022-03-21 23:00:05 +0100

    added corrected header and footer

--- src/components/organisms/PerspectiveA/PerspectiveA.js
+++ src/components/organisms/PerspectiveA/PerspectiveA.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import classes from './PerspectiveA.module.css';
-import TrapezeA from '../../atoms/Decors/TrapezeA/TrapezeA';
+// import TrapezeA from '../../atoms/Decors/TrapezeA/TrapezeA';
 
 const PerspectiveA = (props) => {
   return (
     <div className={classes[props.className] || classes.perspectiveA}>
-      <div className={classes.pointer}></div>
-      <div className={classes.groupA}>
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-        <TrapezeA />
-      </div>
+      {/* <div className={classes.pointer}></div> */}
     </div>
   );
 };
