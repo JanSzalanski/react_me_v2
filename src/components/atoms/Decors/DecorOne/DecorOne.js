823551e2e4031471e05949a73d2f9f66c9bcb48e
JanSzalanski <jan@szalanski.eu>
2022-03-28 23:38:23 +0200

    added DecorOne atom componet

---
+++ src/components/atoms/Decors/DecorOne/DecorOne.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import classes from './DecorOne.module.css';
+
+const DecorOne = (props) => {
+  return (
+    <div className={classes[props.className] || classes.wrapper}>
+      <div className={classes.wrapperB}>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+        <div className={classes.rectangle}></div>
+      </div>
+    </div>
+  );
+};
+
+export default DecorOne;
--- src/pages/ArticlePage/ArticlePage.js
+++ src/pages/ArticlePage/ArticlePage.js
@@ -3,11 +3,14 @@ import Header from '../../components/molecules/Header/Header';
 import Footer from '../../components/molecules/Footer/Footer';
 import classes from './ArticlePage.module.css';
 import Background from '../../components/atoms/Background/Background';
+import DecorOne from '../../components/atoms/Decors/DecorOne/DecorOne';
 
 const ArticlePage = (props) => {
   return (
     <div div className={classes[props.type] || 'page'}>
       <Header>Strona z artykułami</Header>
+      <DecorOne />
+      <DecorOne className="second" />
       <Background />
       <Footer>Stopka</Footer>
     </div>
