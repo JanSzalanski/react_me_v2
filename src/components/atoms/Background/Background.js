79a71086edea8a01a14c38506e965fb875f46996
JanSzalanski <jan@szalanski.eu>
2022-03-28 16:24:18 +0200

    added full border geometry to background element

--- src/components/atoms/Background/Background.js
+++ src/components/atoms/Background/Background.js
@@ -2,7 +2,32 @@ import React from 'react';
 import classes from './Background.module.css';
 
 const Background = (props) => {
-  return <div className={classes.bgWrapper}></div>;
+  return (
+    <div className={classes.bgWrapper}>
+      <div className={classes.lineL}></div>
+      <div className={classes.line1}></div>
+      <div className={classes.line2}></div>
+      <div className={classes.line3}></div>
+      <div className={classes.line4}></div>
+      <div className={classes.line5}></div>
+      <div className={classes.line6}></div>
+      <div className={classes.line7}></div>
+      <div className={classes.line8}></div>
+      <div className={classes.line9}></div>
+      <div className={classes.line10}></div>
+      <div className={classes.lineR}></div>
+      <div className={classes.line11}></div>
+      <div className={classes.line12}></div>
+      <div className={classes.line13}></div>
+      <div className={classes.line14}></div>
+      <div className={classes.line15}></div>
+      <div className={classes.line16}></div>
+      <div className={classes.line17}></div>
+      <div className={classes.line18}></div>
+      <div className={classes.line19}></div>
+      <div className={classes.line20}></div>
+    </div>
+  );
 };
 
 export default Background;
