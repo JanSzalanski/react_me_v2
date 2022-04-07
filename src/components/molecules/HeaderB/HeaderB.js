5eadb470f66a6277e41c6b3468216192cbae3414
JanSzalanski <jan@szalanski.eu>
2022-04-07 23:06:22 +0200

    Probably I recognized an issiue

--- src/components/molecules/HeaderB/HeaderB.js
+++ src/components/molecules/HeaderB/HeaderB.js
@@ -1,30 +1,14 @@
 import React from 'react';
 import classes from './HeaderB.module.css';
-import Heading from '../../atoms/Heading/Heading';
+// import Heading from '../../atoms/Heading/Heading';
 
 const HeaderB = (props) => {
   return (
     <header className={classes[props.className] || classes.wrapperHeader}>
       <div className={classes.line1}></div>
       <div className={classes.line2}></div>
-
       <div className={classes.line3}></div>
-
       <div className={classes.line4}></div>
-
-      <div className={classes.line5}></div>
-
-      <div className={classes.line6}></div>
-
-      <div className={classes.line7}></div>
-
-      <div className={classes.line8}></div>
-
-      <div className={classes.line9}></div>
-
-      <div className={classes.line10}></div>
-
-      <Heading>{props.children}</Heading>
     </header>
   );
 };
