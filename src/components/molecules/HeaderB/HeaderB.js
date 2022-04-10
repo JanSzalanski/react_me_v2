1c2839102c433aa13cc0eeff7d1a811f15eba389
JanSzalanski <jan@szalanski.eu>
2022-04-10 22:44:07 +0200

    another solution look fine for now

--- src/components/molecules/HeaderB/HeaderB.js
+++ src/components/molecules/HeaderB/HeaderB.js
@@ -5,13 +5,16 @@ import classes from './HeaderB.module.css';
 const HeaderB = (props) => {
   return (
     <header className={classes[props.className] || classes.wrapperHeader}>
-      <div className={classes.line1}></div>
-      <div className={classes.line2}></div>
-      <div className={classes.line3}></div>
-      <div className={classes.line4}></div>
-      <div className={classes.line5}></div>
-      <div className={classes.line6}></div>
-      <div className={classes.line7}></div>
+      <div className={classes.innerWrapper}>
+        <div className={classes.line1}></div>
+        <div className={classes.line2}></div>
+        <div className={classes.line3}></div>
+        <div className={classes.line4}></div>
+        <div className={classes.line5}></div>
+        <div className={classes.line6}></div>
+        <div className={classes.line7}></div>
+      </div>
+
       {/* <div className={classes.line8}></div>
       <div className={classes.line9}></div>
       <div className={classes.line10}></div> */}
