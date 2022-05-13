3610a4472f3e5867afd3c1bcdf3146e3b1238853
JanSzalanski <jan@szalanski.eu>
2022-05-13 23:18:27 +0200

    added some lines

--- src/components/molecules/ContactBar/ContactBar.js
+++ src/components/molecules/ContactBar/ContactBar.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import classes from './ContactBar.css';
+import classes from './ContactBar.module.css';
 
 const ContactBar = (props) => {
-  return <div className={classes[props.className] || classes.contactWrapper}></div>;
+  return (
+    <div className={classes[props.className] || classes.contactWrapper}>
+      <div className={classes.lineRight} />
+      <div className={classes.lineLeft} />
+    </div>
+  );
 };
 
 export default ContactBar;
