96de4851b1058c3196cfc8a961e6c3a67618b638
JanSzalanski <jan@szalanski.eu>
2022-05-15 22:02:34 +0200

    added full border lines for contact component

--- src/components/molecules/ContactBar/ContactBar.js
+++ src/components/molecules/ContactBar/ContactBar.js
@@ -6,6 +6,19 @@ const ContactBar = (props) => {
     <div className={classes[props.className] || classes.contactWrapper}>
       <div className={classes.lineRight} />
       <div className={classes.lineLeft} />
+      <div className={classes.lineTop} />
+      <div className={classes.lineBottom} />
+      <div className={classes.line1} />
+      <div className={classes.line2} />
+      <div className={classes.line3} />
+      <div className={classes.line4} />
+      <div className={classes.line5} />
+      {/*  */}
+      <div className={classes.line6} />
+      <div className={classes.line7} />
+      <div className={classes.line8} />
+      <div className={classes.line9} />
+      <div className={classes.line10} />
     </div>
   );
 };
