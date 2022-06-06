c94034637ee81f94df790682452a67474c03c6f0
JanSzalanski <jan@szalanski.eu>
2022-06-06 23:11:36 +0200

    fixed some 3d css

--- src/components/atoms/Label/Label.js
+++ src/components/atoms/Label/Label.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import classes from './Label.module.css';
+import './Label.css';
 import Button from '../Button/Button';
 
 const Label = (props) => {
   return (
-    <label htmlFor={props.htmlFor} id={props.id} className={classes.customLabel}>
+    <label htmlFor={props.htmlFor} id={props.id} className="customLabel">
       {props.children}
       <Button type="secondary">Detale</Button>
     </label>
