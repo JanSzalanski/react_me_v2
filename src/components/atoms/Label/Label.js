f6944fb0d70900f5787948bc3ca75b384b499a05
JanSzalanski <jan@szalanski.eu>
2022-06-08 13:35:14 +0200

    added card component to label componenet and styling it

--- src/components/atoms/Label/Label.js
+++ src/components/atoms/Label/Label.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './Label.css';
-import Button from '../Button/Button';
 
 const Label = (props) => {
   return (
     <label htmlFor={props.htmlFor} id={props.id} className="customLabel" name={props.name}>
       {props.children}
-      <Button type="secondary">Detale</Button>
     </label>
   );
 };
--- src/pages/NewsPage/NewsPage.js
+++ src/pages/NewsPage/NewsPage.js
@@ -5,7 +5,7 @@ import classes from './NewsPage.module.css';
 import Input from '../../components/atoms/Input/Input';
 // import ButtonIcon from '../../components/atoms/ButtonIcon/ButtonIcon';
 // import Pargraph from '../../components/atoms/Paragraph/Paragraph';
-// import Card from '../../components/molecules/Card/Card';
+import Card from '../../components/molecules/Card/Card';
 // import Footer from '../../components/molecules/Footer/Footer';
 import FooterB from '../../components/molecules/FooterB/FooterB';
 // import { Link } from 'react-router-dom';
@@ -31,47 +31,51 @@ const NewsPage = (props) => {
           <input type="radio" name="slider" id="s5" />
 
           <Label htmlFor="s1" id="slide1" name="slide1">
-            Card 1
+            <Card
+              path="a1"
+              date="01/02/2022"
+              title="Pierwszy news"
+              content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus veritatis molestias
+              earum accusamus iste! Corrupti ut ea sint modi magnam atque, dolorum reprehenderit
+              voluptatibus minus totam eos accusamus dolores adipisci!"
+            />
           </Label>
+
           <Label htmlFor="s2" id="slide2" name="slide2">
-            Card 2
+            <Card
+              path="a2"
+              date="02/04/2022"
+              title="Drugi news"
+              content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus veritatis molestias
+               earum accusamus iste! Corrupti ut ea sint modi magnam atque, dolorum reprehenderit"
+            />
           </Label>
           <Label htmlFor="s3" id="slide3" name="slide3">
-            Card 3
+            <Card
+              path="a3"
+              date="25/04/2022"
+              title="Trzeci news"
+              content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus veritatis molestias
+              earum accusamus iste! Corrupti ut ea sint modi magnam !"
+            />
           </Label>
           <Label htmlFor="s4" id="slide4" name="slide4">
-            Card 4
+            <Card
+              path="a4"
+              date="25/05/2022"
+              title="Czwarty news"
+              content="To jest troche treści po polsku"
+            />
           </Label>
           <Label htmlFor="s5" id="slide5" name="slide5">
-            Card 5
+            <Card
+              path="a5"
+              date="01/06/2022"
+              title="Piąty news"
+              content="To jest troche więcej treści po polsku... Tak aby było wiadomo co gdzie i jak."
+            />
           </Label>
         </section>
-        {/* <Card
-          className="hidden"
-          path="a1"
-          date="01/02/2022"
-          title="Pierwszy news"
-          content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus veritatis molestias
-        earum accusamus iste! Corrupti ut ea sint modi magnam atque, dolorum reprehenderit
-        voluptatibus minus totam eos accusamus dolores adipisci!"
-        />
-
-        <Card
-          path="a2"
-          date="02/04/2022"
-          title="Drugi news"
-          content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus veritatis molestias
-        earum accusamus iste! Corrupti ut ea sint modi magnam atque, dolorum reprehenderit"
-        />
-
-        <Card
-          className="hidden"
-          path="a3"
-          date="25/04/2022"
-          title="Trzeci news"
-          content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus veritatis molestias
-        earum accusamus iste! Corrupti ut ea sint modi magnam !"
-        /> */}
       </div>
       <FooterB>
         Jan Szałański <br />
