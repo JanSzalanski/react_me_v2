ceb2d4d03cd1a4aaef44a7fef763aa0ad39b7640
JanSzalanski <jan@szalanski.eu>
2022-08-04 23:30:04 +0200

    added css for second button of menu mobile

--- src/components/atoms/ButtonM/ButtonM.js
+++ src/components/atoms/ButtonM/ButtonM.js
@@ -4,10 +4,6 @@ import classes from './ButtonM.module.css';
 const ButtonM = (props) => {
   return (
     <div className={classes.buttonWrapper}>
-      <div className={classes.glow}>
-        <div className={classes.copy}></div>
-      </div>
-
       <button
         className={classes[props.className] || classes.buttonM}
         type={props.type || 'buttonM'}
@@ -15,6 +11,17 @@ const ButtonM = (props) => {
       >
         <p>{props.children}</p>
       </button>
+      {props.className === 'new' && (
+        <div className={classes.glow}>
+          <div className={classes.copy}></div>
+        </div>
+      )}
+
+      {props.className === 'art' && (
+        <div className={classes.glowA}>
+          <div className={classes.copyA}></div>
+        </div>
+      )}
     </div>
   );
 };
