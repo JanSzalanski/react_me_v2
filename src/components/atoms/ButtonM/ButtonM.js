f36e78e11688d598db2daec1c63a51557d277210
JanSzalanski <jan@szalanski.eu>
2022-08-04 22:31:20 +0200

    added better css for mobile button

--- src/components/atoms/ButtonM/ButtonM.js
+++ src/components/atoms/ButtonM/ButtonM.js
@@ -3,13 +3,19 @@ import classes from './ButtonM.module.css';
 
 const ButtonM = (props) => {
   return (
-    <button
-      className={classes[props.className] || classes.buttonM}
-      type={props.type || 'buttonM'}
-      onClick={props.onClick}
-    >
-      <p>{props.children}</p>
-    </button>
+    <div className={classes.buttonWrapper}>
+      <div className={classes.glow}>
+        <div className={classes.copy}></div>
+      </div>
+
+      <button
+        className={classes[props.className] || classes.buttonM}
+        type={props.type || 'buttonM'}
+        onClick={props.onClick}
+      >
+        <p>{props.children}</p>
+      </button>
+    </div>
   );
 };
 
