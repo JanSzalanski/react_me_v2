7b53e4fcbbc3b97c3aad55c3b01bda355307a8e1
JanSzalanski <jan@szalanski.eu>
2022-09-22 22:38:49 +0200

    added dummy database & backend for comments (firebase)

--- src/components/comments/Comments/Comments.js
+++ src/components/comments/Comments/Comments.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from '../Comments/Comments.module.css';
 import CommentList from '../CommentList/CommentList';
 import Filter from '../Filter/Filter';
@@ -8,11 +8,36 @@ import ZoneMiddle from '../../UI/Zones/ZoneMiddle';
 const Comments = () => {
   const [commentArr, setCommentArr] = useState([]);
 
+  //połaczenie z firebase tylko raz zaraz po wyrenderowaniu komponentu i to dzieki useEffect z pusta tablicą jako drugi argument - jeszcze bez catch & err
+  useEffect(() => {
+    fetch('https://react-dummy-base-default-rtdb.europe-west1.firebasedatabase.app/comments.json')
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedComments = [];
+        for (const key in responseData) {
+          loadedComments.push({
+            id: key,
+            name: responseData[key].name,
+            contents: responseData[key].contents,
+          });
+        }
+        setCommentArr(loadedComments);
+      });
+  }, []);
+
+  //połaczenie z firebase ustawienie metody przesyłu POST i przesłanie dodawanego komentarza jeszcze bez catch & err
   const addCommentHandler = (comment) => {
-    setCommentArr((prevComments) => [
-      ...prevComments,
-      { id: Math.random().toString(), ...comment },
-    ]);
+    fetch('https://react-dummy-base-default-rtdb.europe-west1.firebasedatabase.app/comments.json', {
+      method: 'POST',
+      body: JSON.stringify(comment),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setCommentArr((prevComments) => [...prevComments, { id: responseData.name, ...comment }]);
+      });
   };
 
   const removeCommentHandler = (commentId) => {
