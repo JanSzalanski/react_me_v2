cfe2f0934f3354fb1151e4cfe85fec2e5631c099
JanSzalanski <jan@szalanski.eu>
2022-10-17 23:32:19 +0200

    modified rwd of comment list

--- src/components/comments/CommentList/CommentList.js
+++ src/components/comments/CommentList/CommentList.js
@@ -4,12 +4,16 @@ import Button from '../../atoms/Button/Button';
 
 const CommentList = (props) => {
   return (
-    <section className={classes.commmentList}>
+    <>
       <ul className={classes.ul}>
         {props.comments.map((comment) => (
           <li className={classes.comment} key={comment.id}>
-            <span className={classes.name}>{comment.name}:</span>
-            <span className={classes.contents}>{comment.contents}</span>
+            <div className={classes.nameWrap}>
+              <h2 className={classes.name}>{comment.name} :</h2>
+            </div>
+            <div className={classes.contentWrap}>
+              <p className={classes.content}>{comment.contents}</p>
+            </div>
             <div className={classes.btnWrap}>
               <Button type="buttonFlex" onClick={props.onRemoveItem.bind(this, comment.id)}>
                 Usuń
@@ -20,7 +24,7 @@ const CommentList = (props) => {
       </ul>
       {props.children}
       {props.loading && <h2 className={classes.info}>Pobieranie</h2>}
-    </section>
+    </>
   );
 };
 
