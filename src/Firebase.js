cf964327276b86d240857a3f3e2344500a8d7d58
JanSzalanski <jan@szalanski.eu>
2022-11-21 20:02:06 +0100

    addresed var issiue

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,14 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREABASE_APY_KEY,
-  authDomain: process.env.FIREABASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREABASE_DATA_BASE_URL,
-  projectId: process.env.FIREABASE_PROJECT_ID,
-  storageBucket: process.env.FIREABASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREABASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREABASE_APP_ID,
-  measurementId: process.env.FIREABASE_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_FIREABASE_APY_KEY,
+  authDomain: process.env.REACT_APP_FIREABASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREABASE_DATA_BASE_URL,
+  projectId: process.env.REACT_APP_FIREABASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREABASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREABASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREABASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREABASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
